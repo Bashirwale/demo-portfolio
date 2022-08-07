ab29e0c7171494712d338a7e37e538ffbecb35cc
bashir wale <bashabdulbash@gmail.com>
2022-08-08 00:45:15 +0200

    more update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//opening and closing of mobile by toggling
 const newYear = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 newYear.textContent = currentYear;
@@ -6,4 +7,29 @@ const header = document.querySelector(".header-container");
 const toggleBtn = document.querySelector(".small-menu");
 toggleBtn.addEventListener("click", function () {
   header.classList.toggle("small-screen-header");
+});
+
+//SMOOTH SCROLLING
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+    //closing mobile menu
+    if (link.classList.contains("link"))
+      header.classList.toggle("small-screen-header");
+  });
 });
